132499f2657507babfb44d248938b21cf7e2bb89
ok <qx.ouyang@gmail.com>
2015-12-24 09:51:18 +0800

    Imported user code snippets

---
+++ class.m
@@ -0,0 +1,36 @@
+// class
+// 
+//
+// IDECodeSnippetCompletionPrefix: class
+// IDECodeSnippetCompletionScopes: [StringOrComment]
+// IDECodeSnippetIdentifier: FFD8F913-926E-40A8-8936-1DF5D5D0121A
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+//
+//  name.swift
+//  Main
+//
+//  Created by ok on 15/12/24.
+//  Copyright © 2015年 ok. All rights reserved.
+//
+import UIKit
+import Foundation
+class <#name#> {
+    ///////////////////////////////////properties start/////////////////////////////
+    ///////////////////////////////////properties end///////////////////////////////
+    
+    
+    
+    
+    ///////////////////////////////////super methods start//////////////////////////
+    ///////////////////////////////////super methods end////////////////////////////
+    
+    
+    
+    
+    //////////////////////////////////self methods start///////////////////////////
+    //////////////////////////////////self methods end/////////////////////////////
+    
+
+}
